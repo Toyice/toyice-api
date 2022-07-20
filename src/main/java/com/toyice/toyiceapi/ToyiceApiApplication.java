a3af7fef23684363fe03a3eb34b55497cbc2058e
seoh02h <seok02h@khu.ac.kr>
2022-07-20 16:46:32 +0900

    Add project settings

---
+++ src/main/java/com/toyice/toyiceapi/ToyiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.toyice.toyiceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyiceApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ToyiceApiApplication.class, args);
+  }
+
+}
